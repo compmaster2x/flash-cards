0ce726893e6af140e936fb5881937a07aa8e05a3
compmaster2x <111523497+compmaster2x@users.noreply.github.com>
2025-05-03 17:11:17 +0300

    fixed bugs with gongrats showing and random consequences

--- script.js
+++ script.js
@@ -1,72 +1,69 @@
 //script.js
-import cards from "./data/info.js";
-  
+  import cards from "./data/info.js";
+  import { shuffleCards } from "./components/math.js";
+
+
   let current = 0;
   const front = document.getElementById("front");
   const back = document.getElementById("back");
   const card = document.getElementById("card");
   const nextBtn = document.getElementById("nextBtn");
-  const prevBtn = document.getElementById("prevBtn");
+  
   const rememberButt = document.getElementById("remembered")
   const score = document.getElementById("score")
-  const currentCard = cards[current]
+
 
   function updateCard() {
-    let attempts = 0;
-    while (cards[current]?.isRemembered) {
-      current = (current + 1) % cards.length;
-      attempts++;
-      if (attempts > cards.length) {
-        front.textContent = "🎉 All cards rememorised!";
-        back.textContent = "";
-        score.innerHTML = `Score: 10/10`
-        return;
-      }
-    }
-  
     front.textContent = cards[current].question;
     back.textContent = cards[current].answer;
     card.classList.remove("flipped");
-  
     displayScore();
   }
   
 
   function displayScore(){
-    let count = 0
-    cards.forEach((card) => {
-        if (card.isRemembered) count++ 
-    })
-
-    score.innerHTML = `Score: ${count}/${cards.length}`
+    const rememberedCount = cards.filter(card => card.isRemembered).length
+    score.innerHTML = `Score: ${rememberedCount}/${cards.length}`
   }
+
+  function showCongratsMessage(){
+    front.textContent = "🎉 Все карточки выучены!"
+    back.textContent = "";
+    score.innerHTML = `Score: ${cards.length}/ ${cards.length}`
+    card.classList.remove("flipped")
+  }
+
+  function handleNextCard(){
+    const newIndex = shuffleCards(cards, current)
+    if(newIndex === -1){
+      showCongratsMessage()
+      return
+    }
+    
+    current = newIndex
+    updateCard()
+
+  }
+
+  function handleRemembered(){
+    cards[current].isRemembered = true
+
+    const rememberedCount = cards.filter(card => card.isRemembered).length
+    if(rememberedCount === cards.length){
+      showCongratsMessage()
+      return 
+    }
+    handleNextCard()
+  }
+
+  function toggleFlip(){
+    card.classList.toggle("flipped")
+  }
+
   
-  card.addEventListener("click", () => {
-    card.classList.toggle("flipped");
-  });
-  
-  nextBtn.addEventListener("click", () => {
-    let start = current;
-    do {
-      current = (current + 1) % cards.length;
-    } while (cards[current].isRemembered && current !== start);
-  
-    updateCard();
-  });
-  
-  prevBtn.addEventListener("click", () => {
-    let start = current;
-    do {
-      current = (current - 1 + cards.length) % cards.length;
-    } while (cards[current].isRemembered && current !== start);
-  
-    updateCard();
-  });
-  
-  rememberButt.addEventListener("click", () => {
-    cards[current].isRemembered = true;
-    updateCard();
-  })
+  card.addEventListener("click", toggleFlip)
+  nextBtn.addEventListener("click", handleNextCard)
+  rememberButt.addEventListener("click", handleRemembered)
 
   updateCard();
-  displayScore()
+ 
